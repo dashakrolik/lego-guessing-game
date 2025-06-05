7a0f98ffc89dad5faba47acc45f7d49588baa2b7
Daria Vilevna Epaneshnikova <dariavilevnaepaneshnikova@am-pk4v36hw04.dhcp.nieu.nl.ssg>
2025-06-05 13:34:40 +0200

    improve lego_api, add caching

--- lego_api.py
+++ lego_api.py
@@ -8,22 +8,40 @@ load_dotenv()
 API_KEY = os.getenv("REBRICKABLE_API_KEY")
 BASE_URL = "https://rebrickable.com/api/v3/lego/sets/"
 HEADERS = { "Authorization": f"key {API_KEY}" }
+_theme_cache: dict[int, str] = {}
 
+def get_theme_name(theme_id: int) -> str:
+    if theme_id in _theme_cache:
+        return _theme_cache[theme_id]
+
+    theme_url = f"https://rebrickable.com/api/v3/lego/themes/{theme_id}/"
+    response = requests.get(theme_url, headers=HEADERS, verify=False)
+    if response.status_code != 200:
+        theme_name = f"Unknown Theme ({theme_id})"
+    else:
+        data = response.json()
+        theme_name = data.get("name", f"Theme {theme_id}")
+
+    _theme_cache[theme_id] = theme_name
+    return theme_name
 
 def get_random_set():
-    response = requests.get(f"{BASE_URL}?page_size=1&ordering=random", headers=HEADERS)
+    response = requests.get(f"{BASE_URL}?page_size=1&ordering=random", headers=HEADERS, verify=False)
     response.raise_for_status()
     results = response.json().get("results", [])
+
     if not results:
         return None
 
     raw_set = results[0]
+    theme_name = get_theme_name(raw_set['theme_id'])
 
-    # Construct LegoSet object
     return LegoSet(
         name=raw_set['name'],
         year=raw_set['year'],
         piece_count=raw_set['num_parts'],
-        theme=raw_set['theme_id']  # We'll improve this later
+        theme=theme_name,
+        image_url=raw_set.get('set_img_url')
     )
+
 
