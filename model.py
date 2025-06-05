863839f2d1fe112093a88732fa8b3cbc9404c596
Daria Vilevna Epaneshnikova <dariavilevnaepaneshnikova@am-pk4v36hw04.dhcp.nieu.nl.ssg>
2025-06-05 13:22:46 +0200

    add types to LegoSet class

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 class LegoSet:
-    def __init__(self, name, year, piece_count, theme):
-        self.name = name
-        self.year = year
-        self.piece_count = piece_count
-        self.theme = theme
+    def __init__(self, name: str, year: int, piece_count: int, theme: str):
+        self.name: str = name
+        self.year: int = year
+        self.piece_count: int = piece_count
+        self.theme: str = theme
 
-    def summary(self):
+    def summary(self) -> str:
         return f"{self.name} ({self.year}) - {self.piece_count} pieces - Theme: {self.theme}"
